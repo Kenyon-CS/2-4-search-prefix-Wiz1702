856aa5c917757d231d51c7678f3108e5d1a50912
Wiz1702 <151788806+Wiz1702@users.noreply.github.com>
2024-02-05 23:29:51 -0500

    Update SearchPrefix.rb

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,7 +1,17 @@
-def searchPrefix(arr,s)
-
+def searchItems(arr, starts_with)
+  # Using the select method to filter items that start with the specified string
+  result = arr.select { |item| item.start_with?(starts_with) }
+  result
 end
 
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
+# Examples:
+arr3= ["apple", "banana", "orange", "grape", "pear","apricot"]
+starts_with = "ap"
+puts "Items in arr3 starting with '#{starts_with}': #{searchItems(arr3, starts_with)}"
+arr1 = ["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"]
+starts_with1 = "ab"
+puts "Items in arr1 starting with '#{starts_with1}': #{searchItems(arr1, starts_with1)}"
 
-print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
+arr2 = ["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"]
+starts_with2 = "b"
+puts "Items in arr2 starting with '#{starts_with2}': #{searchItems(arr2, starts_with2)}"
